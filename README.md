db6e06cb753af0f401a3aac7001073dbd000bfe2
Patrick Remy <remy@divera.gmbh>
2021-02-26 15:54:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# BuildKit missing layer bug example
+
+This repository aims to easily reproduce a current BuildKit bug (at least
+reproducible since [`dda009a`](https://github.com/moby/buildkit/commit/dda009a58c76e3e54b4539dce23623eefcc4428c)).
+
+## 🐛 Bug description
+When using raw buildctl commands to build this Dockerfile and export caches,
+then import them, there is a big chance that the resulting image misses the
+`/docker-entrypoint.sh`.
+
+## Reproduce it!
+> HINT: Ensure to be an enough priviliged user, to delete files and folders
+> created by the buildkit container.
+
+Clone this repository and run `./build-script.sh`. It will take some time, to
+execute the required steps. On finish it will either:
+- Exit 1 `image/docker-entrypoint.sh is missing!`, the resulting image will be
+  in the `image` directory.
+- Exit 0 `Reached max number of retries to reproduce issue, setup seems not to be affected`,
+  either the used buildkit version or some modifications on Dockerfile or files
+  in the build context lead to a successful build.
+
+You can adjust the used buildkit image by setting the environment variable
+`BUILDKIT_IMAGE=your/buildkit:12345-rootless`.
+
+## ⚠️ Caution when modifying the Dockerfile
+In my personal experience of reproducing, when trying to delete unused stages
+or layers, or simplify the Dockerfile the script no more fails.
+Sometimes also not `BUILD_PHP_TYPE=cron`, but instead `BUILD_PHP_TYPE=fpm` fails.
---
+++ build-script.sh
@@ -0,0 +1,92 @@
+#/bin/sh
+
+set -e
+
+buildkit_image=${BUILDKIT_IMAGE:-moby/buildkit:v0.8.1-rootless}
+image_target=${BUILD_PHP_TYPE:-cron}
+
+# Build image
+build() {
+  docker run \
+      -it \
+      --rm \
+      --privileged \
+      -v $PWD:/tmp/work \
+      --entrypoint='/bin/sh'  \
+      $buildkit_image \
+        -c "
+          /usr/bin/buildctl-daemonless.sh build \
+            --frontend dockerfile.v0 \
+            --local context=/tmp/work \
+            --local dockerfile=/tmp/work/dockerfiles \
+            --opt filename=php/Dockerfile \
+            --opt target=php-dependencies \
+            --import-cache type=local,mode=max,src=/tmp/work/buildkit-import.cache/1 \
+            --export-cache type=local,mode=max,dest=/tmp/work/buildkit.cache/1 \
+        && \
+        /usr/bin/buildctl-daemonless.sh build \
+          --frontend dockerfile.v0 \
+          --local context=/tmp/work \
+          --local dockerfile=/tmp/work/dockerfiles \
+          --opt filename=php/Dockerfile \
+          --opt target=${image_target}-base \
+          --import-cache type=local,mode=max,src=/tmp/work/buildkit-import.cache/2 \
+          --export-cache type=local,mode=max,dest=/tmp/work/buildkit.cache/2 \
+        && \
+          /usr/bin/buildctl-daemonless.sh build \
+          --frontend dockerfile.v0 \
+          --local context=/tmp/work \
+          --local dockerfile=/tmp/work/dockerfiles \
+          --opt filename=php/Dockerfile \
+          --opt target=${image_target}-production \
+          --import-cache type=local,mode=max,src=/tmp/work/buildkit.cache/1 \
+          --import-cache type=local,mode=max,src=/tmp/work/buildkit.cache/2 \
+          --output type=tar,dest=/tmp/work/image.tar
+        "
+}
+
+echo "Prepare current folder permissions for unprivileged container"
+chmod 777 .
+
+
+if [ "$1" != "skip-create-cache" ]; then
+  echo "Cleanup caches and image"
+  [ ! -f image.tar ] || rm image.tar
+  [ ! -d image ] || rm -rf image
+  [ ! -d buildkit-import.cache ] || rm -rf buildkit-import.cache
+  [ ! -d buildkit.cache ] || rm -rf buildkit.cache buildkit-import.cache
+
+  echo "Create cache (and image) from scratch"
+  build
+
+  echo "Extracting image.tar"
+  [ -d image ] || mkdir image
+  tar -xf image.tar -C image
+
+  [ -f image/docker-entrypoint.sh ] || { echo "image/docker-entrypoint.sh is missing!"; exit 1; }
+  echo "image/docker-entrypoint.sh exists – expected, cleanup"
+else
+  echo "Skipping creating caches from scratch"
+fi
+
+
+# Retry, as sometimes only the second try creates the broken image
+for retry in 1 2; do
+  echo "Cleanup image and rename cache for import"
+  [ ! -f image.tar ] || rm image.tar
+  [ ! -d image ] || rm -rf image
+  [ ! -d buildkit-import.cache ] || rm -rf buildkit-import.cache
+  [ ! -d buildkit.cache ] || mv buildkit.cache buildkit-import.cache
+
+  echo "Build it again, and import caches"
+  build
+
+  echo "Extracting image.tar"
+  [ -d image ] || mkdir image
+  tar -xf image.tar -C image;
+
+  [ -f image/docker-entrypoint.sh ] || { echo "image/docker-entrypoint.sh is missing!"; exit 1; }
+  echo "image/docker-entrypoint.sh exists – not expected, retry"
+done
+
+echo "Reached max number of retries to reproduce issue, setup seems not to be affected"
